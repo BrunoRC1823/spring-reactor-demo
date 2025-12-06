e21791205385c3ac4b1fe40937691fe23fdf2109
William Rivera <riverawilliam2011@gmail.com>
2025-12-06 16:39:39 -0500

    finish methods basics webflux@

--- src/main/java/com/example/springreactordemo/SpringReactorDemoApplication.java
+++ src/main/java/com/example/springreactordemo/SpringReactorDemoApplication.java
@@ -8,8 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 
 @SpringBootApplication
@@ -49,6 +51,53 @@ public class SpringReactorDemoApplication implements CommandLineRunner {
                 .subscribe(a -> log.info("Number: {}", a));
     }
 
+    private void m5delayElements() throws InterruptedException {
+        Flux<Integer> fx = Flux.range(0, 5);
+        fx.delayElements(Duration.ofMillis(500)).doOnNext(a -> log.info("Number: {}", a)).subscribe();
+        Thread.sleep(2700);
+    }
+
+    private void m6zipWith() throws InterruptedException {
+        List<String> client = List.of("Client 1", "Client 2", "Client 3");
+        Flux<String> fx1 = Flux.fromIterable(client);
+        Flux<String> fx2 = Flux.fromIterable(dishes);
+        fx1.zipWith(fx2, (a, b) -> a.concat(" - ").concat(b)).subscribe(log::info);
+    }
+
+    private void m7merge() {
+        List<String> client = List.of("Client 1", "Client 2");
+        Flux<String> fx1 = Flux.fromIterable(client);
+        Flux<String> fx2 = Flux.fromIterable(dishes);
+        Flux.merge(fx1, fx2).subscribe(log::info);
+    }
+
+    private void m8filter() {
+        Predicate<String> filter = a -> a.startsWith("C");
+        Flux.fromIterable(dishes).filter(filter)
+                .subscribe(log::info);
+    }
+
+    private void m9Take() {
+        Flux<String> fl1 = Flux.fromIterable(dishes);
+        fl1.take(2).subscribe(log::info);
+    }
+
+    private void m10TakeLast() {
+        Flux<String> fl1 = Flux.fromIterable(dishes);
+        fl1.takeLast(2).subscribe(log::info);
+    }
+
+    private void m11DefaultIfEmpty() {
+        Flux<String> fl1 = Flux.fromIterable(new ArrayList<>());
+        fl1.map(String::toUpperCase).filter(a -> a.startsWith("C")).defaultIfEmpty("EMPTY FLUX").subscribe(log::info);
+    }
+    private void m12error() {
+        Flux<String> fl1 = Flux.fromIterable(dishes);
+        fl1.doOnNext(e -> {
+            throw  new ArithmeticException("Arithmetic Exception custom");
+        }).onErrorReturn("Error!!").subscribe(log::info);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(SpringReactorDemoApplication.class, args);
     }
@@ -56,11 +105,33 @@ public class SpringReactorDemoApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         dishes.addAll(List.of("Ceviche", "Papa rellena", "Sopa"));
+        log.info("CREATE MONO");
         createMono();
+        log.info("CREATE FLUX");
         createFlux();
+        log.info("METHOD: 1");
         m1doOnNext();
+        log.info("METHOD: 2");
         m2map();
+        log.info("METHOD: 3");
         m3flatMap();
+        log.info("METHOD: 4");
         m4range();
+        log.info("METHOD: 5");
+        m5delayElements();
+        log.info("METHOD: 6");
+        m6zipWith();
+        log.info("METHOD: 7");
+        m7merge();
+        log.info("METHOD: 8");
+        m8filter();
+        log.info("METHOD: 9");
+        m9Take();
+        log.info("METHOD: 10");
+        m10TakeLast();
+        log.info("METHOD: 11");
+        m11DefaultIfEmpty();
+        log.info("METHOD: 12");
+        m12error();
     }
 }
